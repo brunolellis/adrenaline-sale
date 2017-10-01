3438d27522bf9b7a6e32ce45d8a444bd9dd28829
Bruno Lellis <brunolellis@gmail.com>
2017-10-01 20:17:54 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import feedparser
+
+d = feedparser.parse('https://adrenaline.uol.com.br/forum/forums/for-sale.221/index.rss')
+
+print d['entries'][0]['id']
+print d['entries'][0]['link']
+print d['entries'][0]['title']
+
+print d['entries'][1]['id']
+print d['entries'][1]['link']
+print d['entries'][1]['title']
